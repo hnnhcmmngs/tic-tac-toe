68a56947165fb68639aae619131c01256802467b
hnnhcmmngs <hannahcummings94@gmail.com>
2024-03-22 16:10:55 -0400

    Start creating game board instance
    There needs to be an object to control the instance of the tic-tac-toe
    game that contains all the relevant game board and player information.
    This has been implemented, and since there only needs to be a single
    instance of the game board, the module pattern has been used for the
    game board. So far, the module instance includes a "blank" 2D array to
    represent the board, a Player factory function that is used to create
    two players, each with a name and marker, a reference to the current
    player, and two functions to retrieve the current player and toggle the
    current player, respectively. The module instance only returns these
    two functions to aid in gameplay in the future. None of the variables
    are returned to prevent them from being modified.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-const body = document.querySelector("body");
-const h1 = document.querySelector("h1");
+const gameBoard = (function() {
+    let board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
 
-h1.addEventListener("click", () => {
-    const clicked = document.createElement("p");
-    clicked.textContent = "clicked";
-    body.appendChild(clicked);
-});
+    function Player(name, marker) {
+        let getName = () => name;
+        let getMarker = () => marker;
+        return {getName, getMarker};
+    }
+    let player1 = Player("Player 1", "X");
+    let player2 = Player("Player 2", "O");
+
+    let currentPlayer = player1;
+
+    let getCurrentPlayer = () => currentPlayer;
+
+    let toggleCurrentPlayer = () => currentPlayer = currentPlayer == player1 ? player2 : player1;
+
+    return {getCurrentPlayer, toggleCurrentPlayer}
+})();
