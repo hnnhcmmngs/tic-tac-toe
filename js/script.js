9f50aba995ea056873b196b46fc2994ad5642f9f
hnnhcmmngs <hannahcummings94@gmail.com>
2024-03-22 21:54:03 -0400

    Check for game over status after each move
    If a player makes a winning move or if the game board is entirely
    filled, then the game needs to end. There are two new functions
    that accomplish this. The first, checkPlayerWin, checks all possible
    rows, columns, and diagonals for three in a row after a player move
    to check if the player won. The second, checkTie, checks if the board
    is filled and there are no more possible moves. Both of these functions
    are called after every move, and checkPlayerWin is called first to
    make sure the final move of the game wasn't a winning move.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const gameBoard = (function() {
         ["", "", ""]
     ]
 
+    let gameOver = false;
+
     function Player(name, marker) {
         let getName = () => name;
         let getMarker = () => marker;
@@ -20,12 +22,60 @@ const gameBoard = (function() {
     let toggleCurrentPlayer = () => currentPlayer = currentPlayer == player1 ? player2 : player1;
 
     let markBoard = (i, j) => {
+        if (gameOver) {
+            return;
+        }
         if (board[i][j] == "") {
             board[i][j] = currentPlayer.getMarker();
-            toggleCurrentPlayer();
+            if (checkPlayerWin()) {
+                console.log(`Game over! ${currentPlayer.getName()} wins!`);
+                gameOver = true;
+            } else if (checkTie()) {
+                console.log("Game over! Tie! Nobody wins!");
+                gameOver = true;
+            } else {
+                toggleCurrentPlayer();
+            }
         }
         console.log(board);
     };
 
-    return {getCurrentPlayer, toggleCurrentPlayer, markBoard}
+    let checkPlayerWin = () => {
+        let currMarker = currentPlayer.getMarker();
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] == currMarker && board[i][1] == currMarker && board[i][2] == currMarker) {
+                return true;
+            }
+        }
+        // check columns
+        for (let j = 0; j < 3; j++) {
+            if (board[0][j] == currMarker && board[1][j] == currMarker && board[2][j] == currMarker) {
+                return true;
+            }
+        }
+        // check diagonals
+        if (board[0][0] == currMarker && board[1][1] == currMarker && board[2][2] == currMarker) {
+            return true;
+        }
+        if (board[0][2] == currMarker && board[1][1] == currMarker && board[2][0] == currMarker) {
+            return true;
+        }
+        return false;
+    };
+
+    let checkTie = () => {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === "") {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    let getBoard = () => board;
+
+    return {getCurrentPlayer, toggleCurrentPlayer, markBoard, getBoard}
 })();
